8e94aa174d944052f27073ddcc1dd8c4e959732d
darius <darius134966@gmail.com>
2023-11-22 13:18:20 +0330

    add navbar with catch api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'client';
+  products: any[] = [];
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products')
+      .subscribe((res: any) => {
+        this.products = res.data
+      }, error => {
+        console.log(error)
+      })
+  }
+
+  ngOnDestroy() {
+
+  }
+
 }
